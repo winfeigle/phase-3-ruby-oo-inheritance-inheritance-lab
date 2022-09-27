b10bee9bebe7dcaf8ec6f22b4b5960fdc9806ec7
Win Feigle <win.feigle@gmail.com>
2022-09-26 21:14:50 -0600

    commit tree 1eb685ac791d38d149c0b04ca11ef86aa908face

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,12 @@
 class Student < User
-    @knowledge = []
+    attr_reader :knowledge
 
     def initialize
         @knowledge = []
     end
 
-    def learn(string)
-        @knowledge << string
-    end
-
-    def knowledge
-        @knowledge
+    def learn(knowledge)
+        @knowledge << knowledge
     end
 
 end
